9077eddd690e96eb3c12562070a96bff8f374628
Blas Kolic <blaskolic@ciencias.unam.mx>
2017-10-27 19:35:29 -0500

    Added grid calculation for FTLE and max neighborhood.

--- notebooks/JTFunctions.jl
+++ notebooks/JTFunctions.jl
@@ -4,7 +4,8 @@ module JTFunctions
     import TaylorSeries: NumberNotSeries
 
     export evaluate_neighborhood, circle2, square2,
-           ξmax, anal_vs_taylor2D, area_of_polygon
+           ξmax, anal_vs_taylor2D, area_of_polygon,
+           grid_ξmax, vectorField_plot
 
     # Neighborhood Evaluation
     function evaluate_neighborhood{T<:NumberNotSeries}(δU::Function,ϕ::Taylor1{T},num_vals=100)
@@ -84,5 +85,93 @@ module JTFunctions
 
     # Compare taylorinteg solution `x,y` vs analytical ones `xa`
     anal_vs_taylor2D(x,y,xa) = sqrt.((x - xa[:,1]).^2 + (y - xa[:,2]).^2)
+
+
+    ## PLOTTING & GRIDS
+    #No parallelization is done yet.
+
+    #ξmax grid without the plot.. should the plot be included?
+    function grid_ξmax{T<:Real}(eqs_diff!::Function,t0::T,tmax::T;
+                        xlim::Tuple=(-1,1),ylim::Tuple=(-1,1),num_points::Integer=30,
+                        order_jet::Integer=3,order_taylor::Integer=25,abstol=1e-10)
+        @assert get_numvars() == 2 "ξmax must be 2-dimensional."
+        δx,δy = get_variables()
+
+        xgrid= linspace(xlim...,num_points)
+        ygrid= linspace(ylim...,num_points)
+        heatgrid = zeros(length(xgrid),length(ygrid))
+
+        #Parallelize here!
+        for (i,x) in enumerate(xgrid)
+
+            for (j,y) in enumerate(ygrid)
+                q0TN = [x+δx,y+δy]
+                _,ϕN = taylorinteg(eqs_diff!,q0TN,t0,tmax,order_taylor,abstol);
+                heatgrid[i,j] = ξmax(ϕN)
+            end
+
+        end
+        return xgrid, ygrid, heatgrid'
+    end
+
+    #FTLE scalar field using TaylorIntegration lyapunov expectrum.
+    #TODO: FTLEs could also be made with MY method... (JT of order = 1 jets)
+    function grid_FTLE{T<:Real}(eqs_diff!::Function,t0::T,tmax::T;
+                            xlim::Tuple=(-1,1),ylim::Tuple=(-1,1),num_points::Integer=30,
+                            order_taylor::Integer=25,abstol=1e-10)
+
+        qgrid = linspace(xlim...,num_points)
+        pgrid = linspace(ylim...,num_points)
+
+        FTLE = zeros(num_points,num_points)
+
+        #Parallelize here!
+        for (i,q) in enumerate(qgrid)
+            for (j,p) in enumerate(pgrid)
+
+                x0 = [q,p]
+                _,__,λ = liap_taylorinteg(eqs_diff!,x0,t0,tmax,order_taylor,abstol)
+                FTLE[i,j] = maximum(λ[end,:])
+
+            end
+        end
+
+        return qgrid, pgrid, FTLE'
+    end
+
+    #2D vector Field plotting for an TaylorIntegration defined eqs_motion!
+    #See some options for beautifying (optional)
+    function vectorField_plot(vecField!::Function,t0=0.0;xlim::Tuple=(-1,1),ylim::Tuple=(-1,1),num_points::Int=15)
+        X = linspace(xlim...,num_points)
+        Y = linspace(ylim...,num_points)
+
+        Δr = max(X.step.hi,Y.step.hi)
+        sizeGrid = num_points^2
+
+        #allocation
+        vgrid = Vector{Tuple{Float64,Float64}}(sizeGrid)
+        f_norm = Vector{Tuple{Float64,Float64}}(sizeGrid)
+        normFactor = -Inf
+        dx = zeros(Float64,2)
+
+        for (i,x_) in enumerate(X)
+            for (j,y_) in enumerate(Y)
+
+                k = num_points*(i-1)+j
+                vgrid[k] = (x_,y_)
+                vecField!(t0,vgrid[k],dx)
+                normFactor = max(normFactor, norm(dx))
+                f_norm[k] = Tuple(1.5 * Δr * dx)
+
+            end
+        end
+
+        f_norm = [f_norm[i]./normFactor for i in eachindex(f_norm)]
+
+        #Plotting
+        quiver(vgrid, quiver=f_norm)
+        xlims!(xlim...)
+        ylims!(ylim...)
+    end
 
 end #module
